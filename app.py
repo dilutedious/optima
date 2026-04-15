dcb810f9d0f0dcb6f569f8d7da14d36bad585f19
Julian C <1.mugpie@gmail.com>
2026-04-15 17:00:00 +1000

    login + signup screens (static markup)
    just static cards for now (non functional validation or authentication). during planning it was agreed that login shouldn't be complex or time consuming, so it's just username, password, confirm. (no email, captcha, etc.). pleaceholders are generic ('username' and 'password' and not an arbitrary username) so that testers don't get confused if it is a hint.

--- app.py
+++ app.py
@@ -21,5 +21,15 @@ def splash():
     return render_template("splash.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5050, debug=True)
